50e0dfe2aaaac8e9ec80318386d703b868905e44
Fernando Leonid <fernandoleonid@gmail.com>
2020-05-07 08:08:00 -0300

    base para aula de consumo de API

---
+++ 07-consumoAPI/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consumindo API</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="." method="post">
+        <p>Digite o CEP para encontrar o endereço.</p>
+        <input type="text" placeholder="Digite um cep..." name="cep">
+        <input type="submit">
+        <input type="text" placeholder="rua" name="rua">
+        <input type="text" placeholder="bairro" name="bairro">
+        <input type="text" placeholder="cidade" name="cidade">
+        <input type="text" placeholder="estado" name="estado">
+    </form>
+</body>
+</html>
